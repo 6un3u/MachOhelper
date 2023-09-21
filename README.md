0fd491e87e659bcf2048fc87ccf6ca80b6be076b
6un3u <neungyoung.kim@gmail.com>
2023-09-21 09:07:05 +0900

    README

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# MachOparser
+# MachOhelper
 
-A description of this package.
+Get the hash value of section in segment of MachO file
+        
+USAGE: MachOhelper <file-path> [--seg <seg>] [--sec <sec>]
+
+ARGUMENTS:
+  <file-path>             MachO File path
+
+OPTIONS:
+  --seg <seg>             Segment name (default: __TEXT)
+  --sec <sec>             Section name (default: __text)
+  --version               Show the version.
+  -h, --help              Show help information.
--- Sources/MachOhelper/MachOhelper.swift
+++ Sources/MachOhelper/MachOhelper.swift
@@ -5,7 +5,7 @@ public struct MachOhelper: ParsableCommand {
     
     static public let configuration: CommandConfiguration = CommandConfiguration(
         commandName: "MachOhelper",
-        abstract: "Get the hash of the segment section of MachO file",
+        abstract: "Get the hash value of the section in Segment Command of MachO file",
         discussion: "",
         version: "1.1.0"
     )
@@ -13,10 +13,10 @@ public struct MachOhelper: ParsableCommand {
     @Argument(help: "MachO File path")
     var filePath: String
     
-    @Option(name: [.customLong("seg")], help: "Segment name ex) __TEXT")
+    @Option(name: [.customLong("seg")], help: "Segment name")
     var segment: String = SEG_TEXT
 
-    @Option(name: [.customLong("sec")], help: "Section name ex) __text")
+    @Option(name: [.customLong("sec")], help: "Section name")
     var section: String = SECT_TEXT
     
     public func run() throws {
